21e656fb9c06a8ef10d0158fbd34e17750a34201
Zbigniew Downar <zbyszrom@Zbigniews-iMac.local>
2020-05-05 17:48:11 +0200

    firebase boilerplate

---
+++ src/components/firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDR-sr80wXxLxW3CRqsw_r-cWMqgO2L_zU",
+  authDomain: "gatsby-book-club-f6f2e.firebaseapp.com",
+  databaseURL: "https://gatsby-book-club-f6f2e.firebaseio.com",
+  projectId: "gatsby-book-club-f6f2e",
+  storageBucket: "gatsby-book-club-f6f2e.appspot.com",
+  messagingSenderId: "1052892266535",
+  appId: "1:1052892266535:web:b640dda7ec3d4a5c07ce02",
+  measurementId: "G-XQWCXMFRV3",
+}
+export default firebaseConfig
